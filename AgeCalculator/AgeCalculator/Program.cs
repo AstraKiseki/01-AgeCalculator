a5e0516daa6be708e54d93e8cbb0bffd42e68ba4
Rebecca Mooney <astrakiseki@gmail.com>
2016-03-07 08:58:56 -0800

    Removed whitespace, separated the calculator and close method into commands on their own.

--- AgeCalculator/AgeCalculator/Program.cs
+++ AgeCalculator/AgeCalculator/Program.cs
@@ -10,35 +10,44 @@ namespace AgeCalculator
     {
         static void Main(string[] args)
         {
-            Console.Write("Please enter your birthdate (YYYY/MM/DD): ");  // starting point for brain.  Need to turn DOB into string?)
+            AgeCalculate();
+            Close();
+        }
+        public static void AgeCalculate()
+        {
+            Console.Write("Please enter your birthdate (YYYY/MM/DD): ");
             string input = Console.ReadLine();
             var dateOfBirth = DateTime.Parse(input);
             TimeSpan duration = DateTime.Now.Subtract(DateTime.Parse(input));
 
-            /* Okay, assumptions being made here: A year being 365.25 days, a week being 7 days, hence those numbers.
-            ANYWAY YAY
-    */
+            /* Assumptions being made here: A year being 365.25 days, a week being 7 days, hence those numbers. */
 
             int iDays = duration.Days; // It works!  Credit to Mike F for the .Days bit.
             int iYear = (int)(iDays / 365.25); // Lazy mode for the math, sorry.
             iDays -= (int)(iYear * 365.25);
-            int iWeek = (int)(iDays / 7); // HOW THE FUCK DO I DO THIIIIIIS.  I'm going to borrow.  Surprise, I didn't borrow much.
-            iDays -= (int)(iWeek * 7);
+            int iWeek = iDays / 7;
+            iDays -= (iWeek * 7);
             Console.WriteLine("You are {0} years, {1} weeks, {2} days old!", iYear, iWeek, iDays);
-
+        }
+        public static void Close()
+        {
             Console.ReadKey();
-            Console.WriteLine("Exit? (y/n)"); // for good practice
+            Console.WriteLine("Would you like to run another calculation? (y/n)"); // for good practice
             string userValue = Console.ReadLine();
-
+            userValue = userValue.ToLower();
             if (userValue == "y")
             {
                 Environment.Exit(0);
             }
-
             if (userValue == "n")
             {
-                
+                AgeCalculate();
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid answer.");
             }
         }
     }
 }
+
